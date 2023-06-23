01f5074a0df251a7c98ff4902e7ef780863808bd
edho0219 <edho0219@gmail.com>
2023-06-23 08:48:33 +0700

    Membuat unit data catatan di Delphi

---
+++ Unit5.pas
@@ -0,0 +1,49 @@
+unit Unit5;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ComCtrls, Grids, DBGrids, StdCtrls, ExtCtrls;
+
+type
+  TForm5 = class(TForm)
+    lbl1: TLabel;
+    pnl1: TPanel;
+    lbl2: TLabel;
+    lbl3: TLabel;
+    lbl4: TLabel;
+    lbl5: TLabel;
+    lbl9: TLabel;
+    lbl10: TLabel;
+    lbl11: TLabel;
+    lbl6: TLabel;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Edit5: TEdit;
+    btn1: TButton;
+    btn2: TButton;
+    btn3: TButton;
+    btn4: TButton;
+    btn5: TButton;
+    btn6: TButton;
+    Edit6: TEdit;
+    Edit7: TEdit;
+    dbgrd1: TDBGrid;
+    dtp1: TDateTimePicker;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form5: TForm5;
+
+implementation
+
+{$R *.dfm}
+
+end.
